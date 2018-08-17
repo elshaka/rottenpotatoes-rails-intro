9673f7f0ea6873e4cfd6663e4236461395a75505
Eleazar Meza <bairesdev_emeza@iseatz.com>
2018-08-17 16:49:14 -0400

    Implemented movie sorting by title and release date

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,8 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+
+  def table_header_class(sort_by)
+    params[:sort_by] == sort_by ? 'hilite' : nil
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.search(params)
+    movies = Movie.all
+    movies = movies.order(:title) if params[:sort_by] == 'title'
+    movies = movies.order(:release_date) if params[:sort_by] == 'release_date'
+
+    movies
+  end
 end
